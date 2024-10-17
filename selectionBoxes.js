39ecdbfb221510d0155d1c78923da3064723abb1
ITimonI <47868265+ITimonI@users.noreply.github.com>
2024-10-18 00:00:08 +0200

    selection bar logic

--- selectionBoxes.js
+++ selectionBoxes.js
@@ -1,7 +1,37 @@
+let heatmapLevelValue = 'none';
+let magnitudeLevelValue = 'all';
+let isWholePeriod = false;
+
 function handleSelectionBox(e) {
     console.log(e.value);
 
+    switch (e.value) {
+        case "singleYear":
+            heatmapLevelValue = 'none';
+            isWholePeriod = false;
+            break;
+        case "wholePeriod":
+            // heatmapLevelValue = 'all';
+            isWholePeriod = true;
+            break;
+        case "all":
+            magnitudeLevelValue = 'all';
+
+            break;
+        default:
+            magnitudeLevelValue = e.value;
+            break;
+    }
+
+    if(isWholePeriod){
+        heatmapLevelValue = e.value;
+        if(magnitudeLevelValue === 'all'){
+            heatmapLevelValue = 'all';
+        } else {
+            heatmapLevelValue = magnitudeLevelValue;
+        }
+    }
     const magnitudeLevel = this.value;
-    const year = +slider.value;
-    updateMap(year, magnitudeLevel = 'all', heatmapLevel = 'none')
+    const year = slider.value;
+    updateMap(year, magnitudeLevelValue, heatmapLevelValue)
 }
